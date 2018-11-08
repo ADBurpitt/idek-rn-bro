b8cde8eee68c83fd80b7a9c8fdffcf7b159df8c8
ADBurpitt <aaronburpitt@gmail.com>
2018-11-08 18:02:08 +0000

    Add session test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import Amplify from 'aws-amplify'
+import Amplify, { Auth } from 'aws-amplify'
 import axios from 'axios'
 
 import logo from './logo.svg'
 import './App.css'
 import { poolData, API_URL } from 'config'
+import { Container, Row, Col, Button } from 'reactstrap'
 
 Amplify.configure({
   Auth: poolData
@@ -12,12 +13,16 @@ Amplify.configure({
 
 class App extends Component {
 
-  state = { data: '' }
+  state = { data: '', session: null }
 
   handleClick = async () => {
-    console.log('hey')
     const { data } = await axios.get(`${API_URL}/hello`)
     this.setState({ data })
+  }
+
+  getSession = async () => {
+    const session = Auth.currentSession()
+    this.setState({ session })
   }
 
   render() {
@@ -25,19 +30,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button onClick={this.handleClick}> get data </button>
-          <p>data: { this.state.data }</p>
+          <Container>
+            <Row>
+              <Col><Button>Get IP</Button></Col>
+              <Col>IP:</Col>
+              <Col>{this.state.data}</Col>
+            </Row>
+            <br />
+            <Row>
+              <Col><Button color="primary" >Get Session</Button></Col>
+              <Col>{ this.state.session && <Button color="success">Log Session</Button>}</Col>
+            </Row>
+          </Container>
         </header>
       </div>
     )
