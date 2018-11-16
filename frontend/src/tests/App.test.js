f0f369209484ccb810198e64dcdcf33ecc57b2df
ADBurpitt <aaronburpitt@gmail.com>
2018-11-16 18:59:58 +0000

    Fix test

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { BrowserRouter } from 'react-router-dom'
+
+// import App from 'components/App'
+import Home from 'components/pages/Home'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<Home />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+// TODO: Find out how to actually test App with mock provider etc.
